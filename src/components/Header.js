ea9e6f90ba4bdbeb232b1b8095bd2f773b79bd7f
alinecbsr <alinecbsr@gmail.com>
2020-03-11 22:55:48 -0300

    Consuming Header image and information directly in the API

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,76 @@
-import React from "react";
+import React, { Component } from "react";
+/* import PropTypes from "prop-types"; */
+import instance from "../services/movies";
 
 import Play from "../assets/images/playButton.svg";
 import Add from "../assets/images/add.svg";
-/* import Witcher from "../assets/images/witcher.jpg"; */
 
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="header__container">
-        <h1 className="header__container-heading">Título</h1>
-        <button className="header__container-btnPlay">
-          <img
-            src={Play}
-            className="header__container-btnMyList-play"
-            alt="Play"
-          />
-          Play
-        </button>
+class Header extends Component {
+  state = {
+    poster: undefined
+  };
 
-        <button className="header__container-btnMyList">
-          <img
-            src={Add}
-            className="header__container-btnMyList-add"
-            alt="Add"
-          />
-          My List
-        </button>
-        <p className="header__container-overview">
-          Overview Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus lobortis posuere urna, id consequat ante. Cras venenatis
-          erat sem, a tempus neque fermentum mattis.
-        </p>
-      </div>
-      {/* <img src={Witcher} className="poster" alt="Witcher" /> */}
-      <div className="header--fadeBottom"></div>
-    </header>
-  );
+  async componentDidMount() {
+    const response = await instance.get(
+      "/tv/71446?api_key=224ce27b38a3805ecf6f6c36eb3ba9d0"
+    );
+
+    console.log("teste", response.data);
+
+    this.setState({ poster: response.data });
+  }
+
+  render() {
+    const { poster } = this.state;
+
+    const backgroundStyle = {
+      backgroundSize: "cover",
+      backgroundImage: `url(https://image.tmdb.org/t/p/original//aTRzFS0E6KJGns1WsH5IUsuiUbq.jpg)`,
+      backgroundPosition: "center"
+    };
+
+    return (
+      <>
+        <header style={backgroundStyle} className="header">
+          {poster !== undefined && (
+            <div className="header__container">
+              <h1 className="header__container-heading">
+                {poster.original_name}
+              </h1>
+              <button className="header__container-btnPlay">
+                <img
+                  src={Play}
+                  className="header__container-btnMyList-play"
+                  alt="Play"
+                />
+                Play
+              </button>
+
+              <button className="header__container-btnMyList">
+                <img
+                  src={Add}
+                  className="header__container-btnMyList-add"
+                  alt="Add"
+                />
+                My List
+              </button>
+              <p className="header__container-overview">{poster.overview}</p>
+            </div>
+          )}
+          <div className="header--fadeBottom"></div>
+        </header>
+      </>
+    );
+  }
 }
+
+/* Header.propTypes = {
+  poster: PropTypes.arrayOf(
+    PropTypes.shape({
+      original_name: PropTypes.string,
+      overview: PropTypes.string
+    })
+  ).isRequired
+}; */
+
+export default Header;
